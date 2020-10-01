e3abddc452e0ede3001b3ea5814caa38ed267283
superstarsky <mathieu_hance@hotmail.com>
2020-10-01 22:32:42 +0200

    First commit

---
+++ src/main/java/com/restapi/potatrocapi/PotatrocApiApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.potatrocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatrocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatrocApiApplication.class, args);
+	}
+
+}
