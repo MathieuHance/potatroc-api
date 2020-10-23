c5fd634bd88263635526ce7072a8db6141558076
superstarsky <mathieu_hance@hotmail.com>
2020-10-23 20:46:27 +0200

    Ajout des tests de sql

--- src/main/java/com/restapi/potatrocapi/config/SecurityConfig.java
+++ src/main/java/com/restapi/potatrocapi/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.restapi.potatrocapi;
+package com.restapi.potatrocapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
